35b736f806faa37c6e8031cee99a39c3ae1d326d
Cody Kurtz <17343666+CodyKurtz@users.noreply.github.com>
2021-08-26 12:59:56 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "gke1" {
     source  = "tfe.ckurtz.3607853379.workshops.arctiq.ca/arctiq/gke/google"
-    version = "1.0.3"
+    version = "1.0.4"
 
     # insert required variables here
     cluster_name = var.cluster_name
